c32e0420ea8d478b9d7672d22be4effccc833fb3
ayucristiana <ayucristiana5@gmail.com>
2026-08-18 10:13:51 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-function ubahPesan() {
-    const judul = document.getElementById("salam");
-    judul.textContent = "Terima Kasih Sudah Berkunjung! 🎉";
+function scrollToAbout(){
+
+document.getElementById("about").scrollIntoView({
+behavior:"smooth"
+});
+
 }
+
+const cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+card.addEventListener("mouseover",()=>{
+
+card.style.transform="scale(1.08)";
+
+});
+
+card.addEventListener("mouseout",()=>{
+
+card.style.transform="scale(1)";
+
+});
+
+});
+
+window.addEventListener("scroll",()=>{
+
+const nav=document.querySelector("nav");
+
+if(window.scrollY>50){
+
+nav.style.background="#01243d";
+
+}else{
+
+nav.style.background="rgba(0,0,0,.25)";
+
+}
+
+});
